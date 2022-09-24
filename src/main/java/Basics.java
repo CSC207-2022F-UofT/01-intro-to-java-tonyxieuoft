8301a0ccf65b8713205d2543fa745fdb6fd2906a
tonyxieuoft <xiaohan.xie@mail.utoronto.ca>
2022-09-23 20:38:32 -0400

    Completed TODO tasks for GitHub exercise 1, related to the Chapter 1 Introduction ot Java reading.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -141,6 +146,10 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] storage = to_split.split(" ");
+        for (String s : storage){
+            ret.append(s.charAt(0));
+        }
 
         // Fill in the rest of the body here
 
@@ -171,6 +180,13 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
+        int counter = 0;
+        for (int number: arr){
+            if (counter % 2 == 1){
+                current_sum += number;
+            }
+            counter++;
+        }
         return current_sum;
     }
 
